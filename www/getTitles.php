e3e5294ecefcc5e795df6538a76ac9eb44ba09ee
Steve Cody <steve@sbobo.net>
2012-08-13 14:28:33 -0600

    added votecoutn to titles

--- www/getTitles.php
+++ www/getTitles.php
@@ -13,11 +13,12 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 
 mysql_select_db("sbobonet_kracktube", $dbhandle) or die(mysql_error());
 
-$result = mysql_query("SELECT titles.title_id, titles.title, titles.c_user from titles where titles.media_id=" . $media_id . " order by titles.curr_votes desc");
+$result = mysql_query("SELECT titles.title_id, titles.title, titles.c_user, titles.curr_votes from titles where titles.media_id=" . $media_id . " order by titles.curr_votes desc");
 while ($row2 = mysql_fetch_array($result, MYSQL_NUM))
 {
 ?>
-	<li><span class="button"><a href="#" onClick="vote('<?php printf("%s",$row2[0]); ?>', '<?php echo($media_id); ?>');">vote</a></span>&nbsp;&nbsp;<?php printf("%s",$row2[1]); ?> <div class="createdUser">-<?php printf("%s",$row2[2]); ?></div></li>
+	<li><span class="button"><a href="#" onClick="vote('<?php printf("%s",$row2[0]); ?>', '<?php echo($media_id); ?>');">vote</a></span>&nbsp;&nbsp;<?php printf("%s",$row2[1]); ?> 
+	<div class="titleFooter">-<?php printf("%s",$row2[2]); ?>|<?php printf("%s",$row2[3]); ?> votes</div></li>
 <?php
 }
 ?>
--- www/submittitle.php
+++ www/submittitle.php
@@ -17,4 +17,17 @@ mysql_select_db("sbobonet_kracktube", $dbhandle) or die(mysql_error());
 //echo ("insert into titles (c_user,title,media_id) values(\"Steve\", \"" . $title . "\"," . $media_id . ")");
 
 $result = mysql_query("insert into titles (c_user,title,media_id) values(\"Steve\", \"" . $title . "\"," . $media_id . ")");
+
+
+/*$dbh = new mysqli($hostname, $username, $password, "sbobonet_kracktube");
+
+$stmt = $dbh->prepare("insert into titles (c_user,title,media_id) values(:name, :title, :media_id)");
+$stmt->bindParam(':title', $title);
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':media_id', $media_id);
+
+// insert one row
+$name = 'Steve';
+$stmt->execute();*/
+
 ?>
